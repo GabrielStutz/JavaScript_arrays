ba486f01b7f8e20465381f5fd73e7b02b0bb46d7
Gabriel Vieira Stutz <gabrielstutz100@gmail.com>
2022-11-22 21:07:17 -0300

    Aula 2

---
+++ juntando-salas.js
@@ -0,0 +1,6 @@
+const salaJS = ["Evaldo", "Canis", "Mariana"];
+const salaPython = ["Juliana", "Leonardo", "Raquel"];
+
+const salasUnificadas = salaJS.concat(salaPython);
+
+console.log(salasUnificadas);
